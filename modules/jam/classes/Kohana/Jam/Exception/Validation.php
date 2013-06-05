87f81a32ac9a1e653cc56bf3e50350c6f6712c77
Haralan Dobrev <hkdobrev@gmail.com>
2013-06-05 17:23:23 +0300

    updated jam for Kohana 3.3

---
+++ modules/jam/classes/Kohana/Jam/Exception/Validation.php
@@ -0,0 +1,35 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Resource_Jam_Exception_NotLoaded class
+ * Jam NotLoaded Exception
+ *
+ * @package    Despark/http-resource
+ * @author     Haralan Dobrev
+ * @copyright  (c) 2012 Despark Ltd.
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/legalcode
+ */
+class Kohana_Jam_Exception_Validation extends Kohana_Exception {
+	
+	protected $_model;
+	
+	function __construct($message, $model, $fields = NULL)
+	{
+		$this->_model = $model;
+		
+		$fields[':model'] = $model->meta()->model();
+		$fields[':errors'] = $model->errors();
+		
+		parent::__construct($message, $fields);
+	}
+
+	public function model()
+	{
+		return $this->_model;
+	}
+
+	public function errors()
+	{
+		return $this->_model->errors();
+	}
+}
